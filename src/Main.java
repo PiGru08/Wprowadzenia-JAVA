27493eb8d380e2708e48212ab70e50640acd8282
PiGru08 <grucar77@gmail.com>
2024-09-04 13:42:26 +0200

    Losowanie liczby calkowitej

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Witaj ponownie świecie");
-        double wylosowana = Math.random()*10+1;
+        int wylosowana = (int)(Math.random()*10+1);
 
         System.out.println(wylosowana);
     }
