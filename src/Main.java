b32db400049e506a555381cb75a47f46c2783812
PiGru08 <grucar77@gmail.com>
2024-09-04 13:40:22 +0200

    Losowanie liczby rzeczywistej

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Witaj ponownie świecie");
-        //git init, git config user.email i user.name, git add ., git commit -m ,git remote add i za każdym innym razem git pushgit.a
+        double wylosowana = Math.random()*10+1;
+
+        System.out.println(wylosowana);
     }
 }
