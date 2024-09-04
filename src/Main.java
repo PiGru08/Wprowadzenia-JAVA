65657f4747a3d232fe5e33a964d505c40e679833
PiGru08 <grucar77@gmail.com>
2024-09-04 13:33:20 +0200

    Trzeci commit i sciaga z gita

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Witaj ponownie świecie");
+        //git init, git config user.email i user.name, git add ., git commit -m ,git remote add i za każdym innym razem git pushgit.a
     }
 }
